628441fab7982218a2a98e21476200ff24310d62
josehomero <jose.homero.rios@outlook.com>
2020-09-22 17:14:18 -0700

    basic layout done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import './App.css';
+import { Container } from 'react-bootstrap';
+
+import Header from './components/Header'
 
 function App() {
   return (
     <div className="App">
-      <h1>App</h1>
+      <Container>
+      <button className="btn btn-primary">Home</button>
+      <button className="btn btn-warning">Search</button>
+      <button className="btn btn-success">Random Tweet Finder</button>
+      </Container>
+      <Header/>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+
+class Header extends React.Component {
+    render() {
+        return (<Container>
+            <h1>Twitter Showcase App</h1>
+        </Container>)
+    }
+}
+
+export default Header
