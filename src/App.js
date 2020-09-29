5a6c2ff1301219ace30df71afe86f00f75c255b2
josehomero <jose.homero.rios@outlook.com>
2020-09-28 18:02:34 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import {Route, BrowserRouter as Router, Switch, Link} from "react-router-dom"
 import Home from './Pages/Home'
 import UserTweet from './Pages/UserTweet'
 import RandomTweetFinder from './Pages/RandomTweetFinder'
-
 import Header from './components/Header'
+
 
 function App() {
   return (
@@ -17,16 +17,16 @@ function App() {
             <Link to='/'>Home</Link>
           </li>
           <li>
-          <Link to='/randomtweetfInder'>/randomtweetfInder</Link>
+          <Link to='/randomtweetfinder'>randomtweetfinder</Link>
           </li>
           <li>
-          <Link to='/usertweet'>/usertweet</Link>
+          <Link to='/usertweet'>usertweet</Link>
           </li>
         </ul>
       </nav>
-
+<Header />
       <Switch>
-        <Route path='/randomtweetfInder' component={RandomTweetFinder} />
+        <Route path='/randomtweetfinder' component={RandomTweetFinder} />
         <Route path='/usertweet' component={UserTweet} />
         <Route path='/' component={Home}/>
         </Switch>
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Header from './components/Header'
 
 class Home extends React.Component {
    render(){
        return(<div>
-           <h1>Home</h1>
+           <Header />
        </div>)
    }
 }
