893eb72a6b84ffd3deae1dc6149e7cb2ddeb732c
josehomero <jose.homero.rios@outlook.com>
2020-09-30 18:03:36 -0700

    updated

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,18 +1,23 @@
 import React from 'react'
-import Corvette from './images/corvette.png.jpg'
+import Corvette from '../images/corvette.png.jpg'
 
 class Home extends React.Component {
-Image() {
-    return <img src={Corvette} alt="Logo" />;
-}
 
-   render(){
-       return(<div>
-           <h1>Twitter Showcase App!</h1>
-       <p>{Image()}</p>
-           
-       </div>)
-   }
+    render() {
+        return (<div>
+            <h1>Twitter Showcase App!</h1>
+
+            <img src={Corvette} alt='corvette' width='300' height='300' />
+
+            <h2>
+            This app utilizes data from the Twitter API to allow a person to search for recent
+            tweets of specific Twitter users. There is also a random tweet showcase for my
+            favorite Twitter users. Give it a try and look around!
+            </h2>
+            
+
+        </div>)
+    }
 }
 
 export default Home
