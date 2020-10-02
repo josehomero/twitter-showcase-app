7de08945d3c86493b7493a0c56db2139ac4222c4
josehomero <jose.homero.rios@outlook.com>
2020-10-01 17:38:12 -0700

    updated

--- src/Pages/RandomTweetFinder.js
+++ src/Pages/RandomTweetFinder.js
@@ -2,23 +2,32 @@ import React from 'react'
 import { Container } from 'react-bootstrap'
 
 class RandomTweetFinder extends React.Component {
-   render(){
-    function handleClick(e) {
+    constructor() {
+        super()
+        this.state = {
+            submitted: 'Submitted'
+        }
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    handleClick(e) {
         e.preventDefault()
         return (<div>
             <h2>Searched Tweet</h2>
         </div>)
     }
 
-       return(<div>
-           <Container className='form-row py-1'>
-               <form onClick={handleClick}>
+    render() {
+        return (<div>
+            <Container className='form-row py-1'>
+                <form onClick={this.handleClick}>
                     <input className='form-control' name="search" type="text" placeholder="Search?" />
                     <button className='btn btn-primary'>Submit</button>
                 </form>
             </Container>
-       </div>)
-   }
+            <h2></h2>
+        </div>)
+    }
 }
 
 export default RandomTweetFinder
