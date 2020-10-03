632a0302c6cd2fdef8b60221ff38af4fce8c8d33
josehomero <jose.homero.rios@outlook.com>
2020-10-02 18:24:13 -0700

    updated

--- src/Pages/RandomTweetFinder.js
+++ src/Pages/RandomTweetFinder.js
@@ -5,21 +5,33 @@ class RandomTweetFinder extends React.Component {
     constructor() {
         super()
         this.state = {
-            submit: 'Submitted'
+
         }
         this.submitted = this.submitted.bind(this)
     }
 
     submitted(e) {
         e.preventDefault()
-        return this.state.submit
+        console.log('submitted')
+    }
+
+    handleChange(e) {
+        const {name, value} = e.target
+        this.setState({
+            [name] : value,
+        })
+        console.log(value)
     }
 
     render() {
         return (<div>
             <Container className='form-row py-1'>
                 <form onSubmit={this.submitted}>
-                    <input className='form-control' name="search" type="text" placeholder="Search?" />
+                    <input className='form-control'
+                     name="search" type="text" 
+                     placeholder="Search?"
+                     onChange={this.handleChange}
+                     value={this.state.value} />
                     <button className='btn btn-primary'>Submit</button>
                 </form>
             </Container>
