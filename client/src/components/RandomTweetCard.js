4665eddc413bd04675d488757635fd0b3a18eb3a
josehomero <jose.homero.rios@outlook.com>
2020-11-23 16:37:04 -0800

    updated

--- client/src/components/RandomTweetCard.js
+++ client/src/components/RandomTweetCard.js
@@ -1,5 +1,11 @@
 import React from "react"
 
+entitiesMedia() {
+    if(entities.media.length === 0) {
+        return null
+    } else { return src={entities.media[0].media_url} alt="pic" width='150' height='150'}
+}
+
 function RandomTweetCard({ user, id, fullText, entities }) {
     console.log(entities.media)
     return (
@@ -7,7 +13,7 @@ function RandomTweetCard({ user, id, fullText, entities }) {
             <img src={user.profile_image_url} alt="Profile-pic" />
             <h3>{user.name}</h3> <p>@{user.screen_name}</p>
             <p>{user.description}</p>
-            <img src={entities.media[0].media_url} alt="pic" width='150' height='150'/>
+            <img {entitiesMedia} />
             {<p>{fullText}</p>}
         </div>
     )
